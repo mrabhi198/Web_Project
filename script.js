11f9f1187387c4ebde7b6787fa61099f0eec5b1b
Abhinav Alok <abhinavalok318@gmail.com>
2024-04-14 01:10:00 +0530

    add to cart, cart function and total function added

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function ready(){
         var button=addCart[i];
         button.addEventListener("click",addCartClicked);
     }
+    loadCartItems();
 }
 
 //Remove cart Item
@@ -48,25 +49,60 @@ function removeCartItem(event){
     var buttonClicked =event.target;
     buttonClicked.parentElement.remove();
     updateTotal();
+    saveCartItems();
+    updateCartIcon();
 }
 //Quantity Changed
 function quantityChanged(event){
-    var input=event.target;
-    if(isNaN(input.value) || input.value<=0){
-        input.value=1;
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0){
+        input.value = 1;
     }
     updateTotal();
+    saveCartItems();
+    updateCartIcon();
 }
 
 //Add Cart Function
+function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updateTotal();
+    saveCartItems();
+    updateCartIcon();
+}
 
-
-
-
-
-
-
-
+function addProductToCart(title, price, productImg) {
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == title) {
+            alert("You have already add this item to cart");
+            return;
+        }
+    }
+    var cartBoxContent = `<img src="${productImg}" alt="" class="cart-img">
+    <div class="detail-box">
+        <div class="cart-product-title">${title}</div>
+        <div class="cart-price">${price}</div>
+        <input type="number" name="" id="" value="1" class="cart-quantity">
+    </div>
+    <i class='bx bx-trash-alt cart-remove' ></i>`;
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.append(cartShopBox);
+    cartShopBox.getElementsByClassName('cart-remove')[0]
+    .addEventListener('click', removeCartItem);
+    cartShopBox.getElementsByClassName('cart-quantity')[0]
+    .addEventListener('change', quantityChanged);
+    saveCartItems();
+    updateCartIcon();
+}
 
 //Update Total
 function updateTotal(){
@@ -77,21 +113,24 @@ function updateTotal(){
         var cartBox = cartBoxes[i];
         var priceElement = cartBox.getElementsByClassName('cart-price')[0];
         var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-        var price = parseFloat(priceElement.innerText.replace('RS.',''));
+        var price = parseFloat(priceElement.innerText.replace('₹',''));
         var quantity = quantityElement.value;
         total += price * quantity;
     }
-    //if price contain some cents
-        total = Math.round(total *100)/100;
-        document.getElementsByClassName("total-price")[0].innerText = '$' + total;
+    //if price contain some paisa(in points)
+    total = Math.round(total *100)/100;
+    document.getElementsByClassName('total-price')[0].innerText = '₹' + total;
 
-    }
+    //save total to localstorage
+    localStorage.setItem('cartTotal', total)
+}
+
 
 
 //local storage (keep item in cart when page refresh with localstorage)
 function saveCartItems () {
-    var cartCintent = document.getElementsByClassName('cart-content')[0];
-    var cartBoxes = cartCintent.getElementsByClassName('cart-box');
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
     var cartItems = [];
 
     for (var i = 0; i < cartBoxes.length; i++) {
@@ -110,4 +149,43 @@ function saveCartItems () {
         cartItems.push(item);
     }
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
+}
+
+//load in cart
+function loadCartItems () {
+    var cartItems = localStorage.getItem('cartItems');
+    if (cartItems) {
+        cartItems = JSON.parse(cartItems);
+
+        for(var i = 0; i < cartItems.length; i++){
+            var item = cartItems[i];
+            addProductToCart(item.title, item.price, item.productImg);
+
+            var cartBoxes = document.getElementsByClassName('cart-box');
+            var cartBox = cartBoxes[cartBoxes.length - 1];
+            var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+            quantityElement.value = item.quantity;
+        }
+    }
+
+    var cartTotal = localStorage.getItem('cartTotal');
+    if (cartTotal) {
+        document.getElementsByClassName('total-price')[0].innerText = "₹" + cartTotal;
+    }
+    updateCartIcon();
+}
+
+//quantity count in cart icon
+function updateCartIcon () {
+    var cartBoxes = document.getElementsByClassName('cart-box');
+    var quantity = 0;
+    // var cartIcon = document.getElementById('cart-icon');
+
+    for(var i = 0; i < cartBoxes.length; i++){
+        var cartBox = cartBoxes[i];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        quantity += parseInt(quantityElement.value);
+    }
+    var cartIcon = document.querySelector('#cart-icon');
+    cartIcon.setAttribute("data-quantity", quantity);
 }
