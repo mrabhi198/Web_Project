c4cb1f2565901353687bbe9c7716668cfa141a81
Abhinav Alok <abhinavalok318@gmail.com>
2024-04-14 02:02:59 +0530

    .env

--- server.js
+++ server.js
@@ -19,6 +19,29 @@ app.get("/", (req, res) => {
 //stripe
 let stripeGateway = stripe(process.env.stripe_api);
 
+app.post("/stripe-checkout", async (req, res) => {
+    const lineItems = req.body.items.map((item) => {
+        const unitAmount = parseInt(item.price.replace(/[^o-9.-]+/g, "") * 100);
+        console.log("item-price:", item.price);
+        console.log("unitAmount:", unitAmount);
+        return {
+            price_data: {
+                currency: 'inr',
+                product_data: {
+                    name: item.title,
+                    images: [item.productImg]
+                },
+                unit_amount: unitAmount,
+            },
+            quantity: item.quantity,
+        };
+    });
+    console.log("lineItems:", lineItems);
+
+    //create check session
+    const session = await stripeGateway.checkout.sessions.create
+});
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
-})
+});
