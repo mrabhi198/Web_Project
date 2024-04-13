447dafee029ab4e0988a38b9afac96094d60f3a7
Abhinav Alok <abhinavalok318@gmail.com>
2024-04-14 01:43:05 +0530

    creating server

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import stripe from 'stripe';
+
+//load variables
+dotenv.config();
+
+//start server
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+//home route
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {root: "public" });
+});
+
+//stripe
+let stripeGateway = stripe(process.env.stripe_api);
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
